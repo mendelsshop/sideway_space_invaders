1ecc56dd08e93772fd9cc8bffafd11132362a0fc
mendelsshop <runner@fv-az148-127.xpmdczlkzxuu5e4a40yidixp2h.gx.internal.cloudapp.net>
2022-03-10 00:24:32 +0000

    more cstuf but is till dont fully get how to do iget it compiled but get segfault when i use is_pressed - formmated with black

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, Extension
 
 setup(
-    #...
-    ext_modules=[Extension('keyboard', ['keyboard.c'],),],
+    # ...
+    ext_modules=[
+        Extension(
+            "keyboard",
+            ["keyboard.c"],
+        ),
+    ],
 )
