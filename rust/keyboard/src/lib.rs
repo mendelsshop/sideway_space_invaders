ed46149559dc78bed3db9676f59cec46f786b385
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-03-14 23:10:29 -0400

    input working with rust lib just need to figure out crosscompilation and also need to figure out the right timing Duration::from_secs_f32() in lib.rs

--- rust/keyboard/src/lib.rs
+++ rust/keyboard/src/lib.rs
@@ -1,46 +1,101 @@
 extern crate cpython;
-
+// catch space for shooting implement while loop here instead of in main.py because its not moving the player if the player is not pressing up down or space every frame
 use cpython::{Python, py_module_initializer, py_fn , PyResult, PyObject, PyDict, PyString, PyTuple};
-use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    terminal,
+    ExecutableCommand, execute, Result,
+    cursor::{EnableBlinking}
+};
+use std::time::{Duration, Instant};
+
 py_module_initializer!(keyboard, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
     m.add(py, "is_pressed", py_fn!(py, is_pressed(key: &str)))?;
     println!("keyboard module initialized");
     Ok(())
 });
+
+
+fn read_key(timeout: Duration) -> Option<KeyEvent> {
+    struct RawModeGuard;
+    impl Drop for RawModeGuard {
+        fn drop(&mut self) {
+            terminal::disable_raw_mode().unwrap();
+        }
+    }
+    
+    terminal::enable_raw_mode().unwrap();
+    // EnableBlinking;
+    let _guard = RawModeGuard;
+    let start = Instant::now();
+    let mut offset = Duration::ZERO;
+    while offset <= timeout && event::poll(timeout - offset).unwrap() {
+        if let Event::Key(event) = event::read().unwrap() {
+            return Some(event);
+        }
+        offset = start.elapsed();
+    }
+    return None;
+}
 fn is_pressed(py: Python, key: &str) -> PyResult<bool> {
-    let mut rl = Editor::<()>::new();
-    let mut keys = vec![];
-    loop {
-        let readline = rl.readline("");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(&line);
-                keys.push(line);
-            },
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                return Ok(false);
-            },
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                return Ok(false);
-            },
-            Err(err) => {
-                println!("Error: {:?}", err);
+    print!(">");
+    match read_key(Duration::from_secs_f32(1.0)) {
+        Some(KeyEvent {
+            code: KeyCode::Char('q'),
+            modifiers: KeyModifiers::NONE,
+        }) => {
+            if key == "q" {
+                return Ok(true);
+            } else {
                 return Ok(false);
             }
-        }
-        // check if &key is = to the last key in keys return py<bool>
-        println!("{:?}", keys.last().unwrap());
-        if keys.last().unwrap() == key {
-            return Ok(true);
-        }
-        else {
+        },
+        Some(KeyEvent {
+            code: KeyCode::Up,
+            modifiers: KeyModifiers::NONE,
+        }) => {
+            if key == "up" {
+                return Ok(true);
+            } else {
+                return Ok(false);
+            }
+        },
+        Some(KeyEvent {
+            code: KeyCode::Down,
+            modifiers: KeyModifiers::NONE,
+        }) => {
+            if key == "down" {
+                return Ok(true);
+            } else {
+                return Ok(false);
+            }
+        },
+        Some(KeyEvent {
+            code: KeyCode::Enter,
+            modifiers: KeyModifiers::NONE,
+        }) => {
+            if key == "enter" {
+                return Ok(true);
+            } else {
+                return Ok(false);
+            }
+        },
+        Some(KeyEvent {
+            code: KeyCode::Char(' '),
+            modifiers: KeyModifiers::NONE,
+        }) => {
+            if key == "space" {
+                return Ok(true);
+            } else {
+                return Ok(false);
+            }
+        },
+        _ => {
             return Ok(false);
         }
-
-
+        
     }
+    
+
 }
