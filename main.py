9a95a12a152b8b0b6bdb6d122d4b161d2e0b62ca
Mendel Rubin <82669516+mendelsshop@users.noreply.github.com>
2022-03-16 12:18:19 -0400

    made enemey palceemtn random and made sure (generally) that should be aleast one space ship onon each row

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ bullet_speed = 0.01 if os.name == "posix" else 0.000000000000000001
 # make a variable for the length and height of the board so we can change it for different levels
 # move most of the from here to somewhere else in src/ to make more modular
 # make a function to display the board instead of doing it in three lines each time
-# change it from a dictionary of lists to a list of lists
+# change it from a dictionary of lists to list list of lists
 
 
 def readme():
@@ -22,7 +22,55 @@ def clear(times):
     os.system(clears)
 
 
-    
+def create_board(x, y):
+    board = [
+        [" " for i in range(x)] for j in range(y)
+    ]
+    enemy_pos = []
+    for enemy_y in range(y):
+        enemy_x = random.randrange(x-6, x)
+        enemy_pos.append([enemy_x, enemy_y])
+    board = clear_enemys(board, range(44,51), enemy_pos)
+    return [board, enemy_pos]
+def locate_min(list):
+    smallest = min(list)
+    return [index for index, element in enumerate(list) if smallest == element]
+
+def best_place(enemy_pos):
+    ys = [0, 0, 0, 0, 0]
+    for enemy in enemy_pos:
+        ys[enemy[1]] += 1
+    return random.choice(locate_min(ys))
+def clear_enemys(board, enemy_range, enemy_pos):
+    for row in board:
+        for enemy in enemy_range:
+            if row[enemy] == "<":
+                row[enemy] = " "
+    for enemy in enemy_pos:
+        board[enemy[1]][enemy[0]] = "<"
+    return board
+def spawan_enemys(board, enemy_pos):
+    for i in range(random.randrange(1, 3)):
+        x = random.randrange(44, 51)
+        y = best_place(enemy_pos)
+        if board[y][x] == "<":
+            if y == 4:
+                y = 0
+            else:
+                y += 1
+        enemy_pos.append([x, y])
+    if len(enemy_pos) > 10:
+        while len(enemy_pos) > 5 :
+            enemy_pos.pop()
+    if len(enemy_pos) < 5:
+        for i in range(5-len(enemy_pos)):
+            enemy_pos.append([random.randrange(44, 51), locate_min(enemy_pos)[0]])
+    board = clear_enemys(board, range(44,51), enemy_pos)
+    return [board, enemy_pos]
+
+
+
+
 clear(0)
 print(readme())
 print("press enter to start or q to quit")
@@ -36,34 +84,12 @@ key_p = ""
 player_ps = random.randrange(5)
 shoot = False
 prev_player_pos = None
-enemys = 5
+board, enemy_pos = create_board(51, 5)
 # if the '-' collides with '<' then '<' is destroyed
 # if 'O' reaches the end of the board than you win the game
-def spawan_enemys(dict):
-    for i in range(random.randrange(1, 2)):
-        x = random.randrange(44, 51)
-        y = random.randrange(4)
-        if dict["row" + str(y + 1)][x] == "<":
-            if y == 4:
-                y = 0
-
-            else:
-                y += 1
-
-        dict["row" + str(y + 1)][x] = "<"
-    return dict
-
-
-board = {
-    "row1": [" "] * 50,
-    "row2": [" "] * 50,
-    "row3": [" "] * 50,
-    "row4": [" "] * 50,
-    "row5": [" "] * 50,
-}
 #  for each row in the dictionary 'board' append the '<' to the end of the row
-for row in board:
-    board[row].append("<")
+len_board = len(board)
+
 
 for i in range(51):
     clear(0.1)
@@ -76,20 +102,21 @@ for i in range(51):
     else:
         player_ps = random.randrange(5)
 
-    if board["row" + str(player_ps + 1)][i] == "<":
-        board["row" + str(prev_player_pos + 1)][prev_i] = ""
-        board["row" + str(player_ps + 1)][i] = "⦓"
+    if board[player_ps][i] == "<":
+        board[prev_player_pos][prev_i] = ""
+        board[player_ps][i] = "⦓"
         print("-" * 51)
-        for row in board:
+        for row in range(len_board):
+            board = clear_enemys(board, range(44,51), enemy_pos)
             print("".join(board[row]))
         print("-" * 51)
         print("You lose!")
         break
 
     if prev_player_pos != None:
-        board["row" + str(prev_player_pos + 1)][prev_i] = " "
+        board[prev_player_pos][prev_i] = " "
 
-    board["row" + str(player_ps + 1)][i] = "O"
+    board[player_ps][i] = "O"
     if shoot:
         print("".join(board[rows]))
         print("-" * 51)
@@ -98,26 +125,21 @@ for i in range(51):
         while bulet < 51:
             os.system(clears)
             print("-" * 51)
-            for rowss in board:
-                if board["row" + str(player_ps + 1)] == board[rowss]:
+            for rowss in range(len_board):
+                if board[player_ps] == board[rowss]:
                     # check if the current bulet position is the as "<"
                     if board[rowss][bulet] == "<":
-                        enemys -= 1
-
+                        enemy_pos.remove([bulet, rowss])
+                        board, enemy_pos = spawan_enemys(board, enemy_pos)
                     board[rowss][bulet] = "-"
-
                     if prev_b != 0:
                         board[rowss][prev_b] = " "
-
                     prev_b = bulet
                     bulet += 1
-                if enemys != 5:
-                    board = spawan_enemys(board)
-                    enemys = 5
 
                 if bulet == 51 and board[rowss][bulet - 1] == "-":
                     board[rowss][bulet - 1] = " "
-
+                board = clear_enemys(board, range(44,51), enemy_pos)
                 print("".join(board[rowss]))
                 last_elemnt = len(board[rowss])
 
@@ -126,11 +148,12 @@ for i in range(51):
 
     else:
         print("-" * 51)
-        for rows in board:
+        for rows in range(len_board):
+            board = clear_enemys(board, range(44,51), enemy_pos)
             print("".join(board[rows]))
         print("-" * 51)
 
-    if board["row" + str(player_ps + 1)][50] == "O":
+    if board[player_ps][50] == "O":
         print("You win!")
         break
 
